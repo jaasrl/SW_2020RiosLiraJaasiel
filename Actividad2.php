8b7357818e4aa368362487f3b79604e3fcdea86a
jaasrl <riosjaasiel@comunidad.unam.mx>
2020-05-14 13:35:40 -0500

    Feat: Actividad2 subiendo mi primer archivo. Lo mando dos veces porque en la primera no me guardo todo el archivo php

--- Actividad2.php
+++ Actividad2.php
@@ -2,6 +2,6 @@
   echo "Ríos Lira Jaasiel";
   echo "<ul>";
   echo "<li>Me ha gustado a hacer un apagina html mezclada con php ya que en mi ete solo veimos html</li>";
-  echo "<li>Me intereso el empezar a usar</li>";
+  echo "<li>Me intereso el empezar a usar GitHub ya que me parece una herramienta muy buena para el Open Source </li>";
   echo "</ul>";
  ?>
