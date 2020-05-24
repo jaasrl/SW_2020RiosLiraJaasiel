c9cfee85de9479f314ddadb7ec1d07a24bc60644
jaasrl <riosjaasiel@comunidad.unam.mx>
2020-05-24 14:44:33 -0500

    FEAT: Agregue la Actividad2.php a la carpeta Actividad2

---
+++ Actividad2/Actividad2.php
@@ -0,0 +1,7 @@
+<?php
+  echo "Ríos Lira Jaasiel";
+  echo "<ul>";
+  echo "<li>Me ha gustado a hacer un apagina html mezclada con php ya que en mi ete solo veimos html</li>";
+  echo "<li>Me intereso el empezar a usar GitHub ya que me parece una herramienta muy buena para el Open Source </li>";
+  echo "</ul>";
+ ?>
